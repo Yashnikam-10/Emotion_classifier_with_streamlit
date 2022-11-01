7211f9f187b5d1b5ce6d71f635873ee41644a632
Yashnikam-10 <82996923+Yashnikam-10@users.noreply.github.com>
2022-11-01 12:13:10 +0530

    Added the Monitor Page of the app

--- App/app.py
+++ App/app.py
@@ -5,6 +5,16 @@ import joblib
 import pickle
 import tensorflow as tf
 import altair as alt
+from datetime import datetime
+import plotly.express as px 
+from track_utils import create_page_visited_table,add_page_visited_details,view_all_page_visited_details,add_prediction_details,view_all_prediction_details,create_emotionclf_table
+import sqlite3
+
+
+with sqlite3.connect('data/data.db') as conn:
+    c = conn.cursor()
+    create_emotionclf_table(c)
+    create_page_visited_table(c)
 
 # Load class names
 classNames = np.load("data/class_names.npy")
@@ -40,6 +50,12 @@ def main():
             col1, col2 = st.columns(2)
 
             prediction, probability = predict_emotions(raw_text)
+
+
+            with sqlite3.connect('data/data.db') as conn:
+                c = conn.cursor()
+                add_prediction_details(raw_text,prediction,np.max(probability),datetime.now(), c, conn)
+
             with col1:
                 st.success("Original Text")
                 st.write(raw_text)
@@ -59,10 +75,34 @@ def main():
                 st.altair_chart(fig, use_container_width=True)
 
     elif choice == "Monitor":
-        st.subheader("Monitor App")
+        with sqlite3.connect('data/data.db') as conn:
+            add_page_visited_details("Monitor",datetime.now(), conn.cursor(), conn)
+            st.subheader("Monitor App")
+
+            with st.expander("Page Metrics"):
+                
+                page_visited_details = pd.DataFrame(view_all_page_visited_details(conn.cursor()),columns=['Pagename','Time_of_Visit'])
+                st.dataframe(page_visited_details)	
+
+                pg_count = page_visited_details['Pagename'].value_counts().rename_axis('Pagename').reset_index(name='Counts')
+                c = alt.Chart(pg_count).mark_bar().encode(x='Pagename',y='Counts',color='Pagename')
+                st.altair_chart(c,use_container_width=True)	
+
+                p = px.pie(pg_count,values='Counts',names='Pagename')
+                st.plotly_chart(p,use_container_width=True)
+
+            with st.expander('Emotion Classifier Metrics'):
+                df_emotions = pd.DataFrame(view_all_prediction_details(conn.cursor()),columns=['Rawtext','Prediction','Probability','Time_of_Visit'])
+                st.dataframe(df_emotions)
+
+                prediction_count = df_emotions['Prediction'].value_counts().rename_axis('Prediction').reset_index(name='Counts')
+                pc = alt.Chart(prediction_count).mark_bar().encode(x='Prediction',y='Counts',color='Prediction')
+                st.altair_chart(pc,use_container_width=True)	
 
     else:
         st.subheader("About")
+        add_page_visited_details("About",datetime.now())
+
 
 if __name__ == "__main__":
     main()
